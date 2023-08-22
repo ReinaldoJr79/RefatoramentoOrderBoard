1bd8d7ab4d215fa1f1e42e05e0881d29502e9baf
ReinaldoJr79 <115327655+ReinaldoJr79@users.noreply.github.com>
2023-08-22 20:47:23 -0300

    Add files via upload

---
+++ AppFood-master/fe/src/components/Orders/index.tsx
@@ -0,0 +1,13 @@
+import { Container } from "./styles";
+import { Ordersboard } from "../Ordersboard/index";
+
+export function Orders() {
+    return (
+        <Container>
+            <Ordersboard/>
+            <Ordersboard/>
+            <Ordersboard/>
+            <Ordersboard/>
+        </Container>
+    )
+}
